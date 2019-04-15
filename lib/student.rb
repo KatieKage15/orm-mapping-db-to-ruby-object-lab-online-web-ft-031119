7e9eb60057befa57ae616dd420f8817385ee1448
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-15 08:36:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,10 @@ class Student
       FROM students
       WHERE name = ?
       LIMIT 1
-    SQL
+    SQL 
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def save
